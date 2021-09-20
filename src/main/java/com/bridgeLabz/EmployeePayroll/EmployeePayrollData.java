0de0f35b4157548b8efcf075f6f81eda92f5798b
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-20 20:32:40 +0530

    Added: Employee Payroll Service to read an employee from console

---
+++ src/main/java/com/bridgeLabz/EmployeePayroll/EmployeePayrollData.java
@@ -0,0 +1,15 @@
+package com.bridgeLabz.EmployeePayroll;
+
+public class EmployeePayrollData 
+{
+	private int id;
+	private String name;
+	private double salary;
+	
+	public EmployeePayrollData(int id, String name, double salary)
+	{
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+}
